3a000014d357d6fb28755977bb13c63be0eb4db9
Igor Eskin <github email address>
2018-04-17 13:26:32 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,10 @@ class App extends React.Component {
   // TODO: componentDidMount()
   // TODO: componentWillUnmount()
 
+  componentWillMount() {
+    this.fetchTweets();
+  }
+
   startInterval = () => {
     this.interval = setInterval(this.fetchTweets, 2000);
   }
@@ -36,6 +40,14 @@ class App extends React.Component {
         <TweetWall newTweets={this.state.latestTweets} />
       </div>
     )
+  }
+
+  componentDidMount() {
+    this.startInterval();
+  }
+
+  componentWillUnmount() {
+    this.cleanUpInterval();
   }
 }
 
